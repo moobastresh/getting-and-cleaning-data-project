734b2e12b1ce7f632c6fca916ac759090ef634da
moobastresh <yzzhuyinga@gmail.com>
2016-12-24 23:55:16 -0600

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -14,5 +14,4 @@ d1<-as.numeric(paste0(data1$Global_active_power))
 png(filename = "/Users/yz/Desktop/data science John Hopkins/exploratory data analysis/week1/plot_1.png",
     width = 480, height = 480, units = "px")
 hist(d1, breaks=12, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
-#dev.copy(png, file="/Users/yz/Desktop/data science John Hopkins/exploratory data analysis/week1/plot_1.png")
 dev.off()
