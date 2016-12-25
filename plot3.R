1c447e886b06b01faa56ec5a3bb8225b93357949
moobastresh <yzzhuyinga@gmail.com>
2016-12-25 12:00:45 -0600

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,3 +1,15 @@
+## reading the data
+URL<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(URL,destfile = "power_consumption.zip", method = "curl")
+zipF<-"power_consumption.zip"
+outDir<-"./unzipfolder"
+unzip(zipF,exdir=outDir)
+filenm<-list.files("./unzipfolder")
+setwd("./unzipfolder")
+data<-read.table(filenm, sep=";", header=TRUE)
+data1<-data[data$Date %in% c("1/2/2007","2/2/2007"),]
+d1<-as.numeric(paste0(data1$Global_active_power))
+
 ## generate figure 3
 z1<-as.numeric(paste0(data1$Sub_metering_1))
 z2<-as.numeric(paste0(data1$Sub_metering_2))
